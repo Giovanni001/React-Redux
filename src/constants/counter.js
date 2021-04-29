5e34434a474917de3689ed4a7f13715a0eb5bfba
Giovanni <giovannimanganotti001@gmail.com>
2021-04-29 09:13:41 -0300

    constants

---
+++ src/constants/counter.js
@@ -0,0 +1,11 @@
+/* 
+  Estamos criando um objeto com os tipos de ações que vão ser especificadas no
+  "action.type" no arquivo counter.js da pasta "reducers".
+*/
+
+const actionsTypes = {
+  COUNTER_INCREMENT: "COUNTER_INCREMENT",
+  COUNTER_DECREMENT: "COUNTER_DECREMENT"
+}
+
+export { actionsTypes }
